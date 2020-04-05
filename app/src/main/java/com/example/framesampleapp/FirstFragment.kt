b96700565ab9e97021faee1003bc0b2fecc67e49
Bob Fly <d.a.kryl@gmail.com>
2020-04-05 15:38:52 +0300

    Init Commit

---
+++ app/src/main/java/com/example/framesampleapp/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.example.framesampleapp
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_one.*
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
+class FirstFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        selectionSupreme.text = arguments?.getString("key") ?: "Nan"
+        selectionButton.setOnClickListener {
+
+            selectionSupreme.visibility = View.VISIBLE
+        }
+        navToSecondFragment.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.fragmentContainer, SecondFragment())
+                ?.commit()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/framesampleapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.framesampleapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_one.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fragmentOne = FirstFragment()
+        val fragmentTwo = SecondFragment()
+
+        val someData = Bundle()
+        someData.putString("key", "value")
+       // someData.putInt("intKey", 45)
+        fragmentOne.arguments = someData
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, FirstFragment())
+            .commit()
+//        supreme.setOnClickListener{
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragmentContainer, FirstFragment())
+//                .commit()
+//        }
+//        newBalance.setOnClickListener{
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fragmentContainer, SecondFragment())
+//                .commit()
+//        }
+
+
+
+    }
+
+
+}
